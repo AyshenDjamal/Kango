3a3b624519176a4e599779a715820f3407d58879
AyshenDjamal <ayshen.rasul92@gmail.com>
2025-02-06 19:31:05 +0400

    duzelish edildi

--- src/Kango/Employees.java
+++ src/Kango/Employees.java
@@ -2,20 +2,20 @@ package Kango;
 
 //superClass
 public class Employees {
-    protected static String nameSurname;
-    private  static int age;
-    public static String profession;
-    protected static String location;
+    protected String nameSurname;
+    private  int age;
+    public String profession;
+    protected String location;
 
 
     protected Employees(String nameSurname, int age, String profession, String location) {
-        nameSurname = nameSurname;
-        age = age;
-        profession = profession;
-        location = location;
+        this.nameSurname = nameSurname;
+        this.age = age;
+        this.profession = profession;
+        this.location = location;
     }
 
-    protected static void info() {
+    protected void info() {
         System.out.println("Name and Surname: " + nameSurname + "\nAge: " + age +  "\nProfession: " + profession + "\nLocation: " + location);
     }
 
--- src/departments/cs/CustomerService.java
+++ src/departments/cs/CustomerService.java
@@ -4,28 +4,28 @@ import Kango.Employees;
 
 //Subclass Customer Service
 public class CustomerService extends Employees {
-    private static int employeeCount;
-    static String contactMethod;
+    private final int employeeCount;
+    String contactMethod;
 
 
 
     // Customer Service Konstruktor
     public CustomerService(String nameSurname, int age, String profession, String location, int employeeCount, String contactMethod) {
         super(nameSurname, age, profession, location);
-        employeeCount = employeeCount;
-        contactMethod = contactMethod;
+        this.employeeCount = employeeCount;
+        this.contactMethod = contactMethod;
     }
 
-    protected static void respondToCustomers() {
+    protected void respondToCustomers() {
         System.out.println("They respond to customers " + contactMethod);
     }
 
     //get ve call method yaratdim
-    public static int getEmployeeCount(){ /// niye protected olmadi?
+    public int getEmployeeCount(){ /// niye protected olmadi?
         return employeeCount;
     }
 
-   public static void callRespondToCustomers() { /// call method istifade etdim metodu public etdim
+   public void callRespondToCustomers() { /// call method istifade etdim metodu public etdim
         respondToCustomers();
    }
 }
